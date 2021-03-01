f771d533627281e628bb12972db79ec84c1abde6
RAJ5242 <75838783+RAJ5242@users.noreply.github.com>
2021-03-01 18:49:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var car,wall,speed,weight 
+function setup() {
+  createCanvas(1600,400);
+ car= createSprite(400, 200, 50, 50);
+ wall=createSprite(1500,200,60,200)
+ speed=random(55,90)
+ weight=random(400,1500)
+ car.velocityX=speed
+
+}
+
+function draw() {
+  background(255,255,255);
+  if(wall.x-car.x<(car.width+wall.width)/2)  {
+    car.velocityX=0
+    var d =0.5*weight*speed*speed/22500
+    if(d>180){
+      car.shapeColor="red"
+    }
+    if(d<180&&d>100){
+      car.shapeColor="yellow"
+    }
+    if(d<100){
+      car.shapeColor="green"
+    }
+  }
+  drawSprites();
+}
